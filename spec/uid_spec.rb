b838bf71f3512446191f0fc0c996919ddbc582f6
Simen Svale Skogsrud <simen@bengler.no>
2012-01-25 11:16:11 +0100

    Minor edit

--- spec/uid_spec.rb
+++ spec/uid_spec.rb
@@ -113,9 +113,6 @@ describe Pebblebed::Uid do
     Pebblebed::Uid.valid?("a:b.c.d").should be_true
   end
 
-<<<<<<< HEAD
-end
-=======
   it "knows how to extract the realm from the path" do
     Pebblebed::Uid.new("klass:realm.other.stuff$3").realm.should eq 'realm'
     Pebblebed::Uid.new("klass:realm$3").realm.should eq 'realm'
@@ -123,5 +120,10 @@ end
     Pebblebed::Uid.new("klass:$3").realm.should eq nil
   end
 
+  it "knows how to extract the realm from the path" do
+    Pebblebed::Uid.new("klass:realm.other.stuff$3").realm.should eq 'realm'
+    Pebblebed::Uid.new("klass:realm$3").realm.should eq 'realm'
+    Pebblebed::Uid.new("klass:realm").realm.should eq 'realm'
+    Pebblebed::Uid.new("klass:$3").realm.should eq nil
+  end
 end
->>>>>>> Uid can extract realm
