3b4d3ed838ca4747ea66b40ce7bfda13119aa821
Katrina Owen <katrina.owen@gmail.com>
2012-05-14 16:18:02 +0200

    Add support for our usual uid checks for pebble apis.

---
+++ spec/multi_uid_spec.rb
@@ -0,0 +1,50 @@
+require 'pebblebed/uid'
+require 'pebblebed/multi_uid'
+
+describe Pebblebed::MultiUID do
+  context "with comma separated uids." do
+    subject { Pebblebed::MultiUID.new("xyz:a.b.c$1,xyz:a.b.c$2") }
+
+    its(:list?) { should be_true }
+    its(:wildcard?) { should be_false }
+    its(:one?) { should be_false }
+    its(:uids) { should eq(["xyz:a.b.c$1", "xyz:a.b.c$2"]) }
+
+  end
+
+  context "with a wildcard path." do
+    subject { Pebblebed::MultiUID.new("xyz:a.b.*") }
+
+    its(:list?) { should be_false }
+    its(:wildcard?) { should be_true }
+    its(:one?) { should be_false }
+    its(:uids) { should eq("xyz:a.b.*") }
+  end
+
+  context "without any oid." do
+    subject { Pebblebed::MultiUID.new("xyz:a.b.c") }
+
+    its(:list?) { should be_false }
+    its(:wildcard?) { should be_true }
+    its(:one?) { should be_false }
+    its(:uids) { should eq("xyz:a.b.c") }
+  end
+
+  context "with a wildcard oid." do
+    subject { Pebblebed::MultiUID.new("xyz:a.b.c$*") }
+
+    its(:list?) { should be_false }
+    its(:wildcard?) { should be_true }
+    its(:one?) { should be_false }
+    its(:uids) { should eq("xyz:a.b.c$*") }
+  end
+
+  context "with a completely constrained uid" do
+    subject { Pebblebed::MultiUID.new("xyz:a.b.c$1") }
+
+    its(:list?) { should be_false }
+    its(:wildcard?) { should be_false }
+    its(:one?) { should be_true }
+    its(:uids) { should eq("xyz:a.b.c$1") }
+  end
+end
