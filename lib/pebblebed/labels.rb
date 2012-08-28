724ea793a57983f115b83cdc3e7a675332bc321e
Katrina Owen <katrina.owen@gmail.com>
2012-08-28 14:52:12 +0200

    Add support for stop marker

--- lib/pebblebed/labels.rb
+++ lib/pebblebed/labels.rb
@@ -1,17 +1,23 @@
 module Pebblebed
   class Labels
 
-    attr_reader :path, :prefix, :suffix
+    NO_MARKER = Class.new
+
+    attr_reader :path, :prefix, :suffix, :stop
     def initialize(path, options = {})
       @path = path
       @prefix = options.fetch(:prefix) { 'label' }
       @suffix = options.fetch(:suffix) { nil }
+      @stop = options.fetch(:stop) { NO_MARKER }
     end
 
     def expanded
       values = {}
       path.split('.').each_with_index do |label, i|
         values[label(i)] = label
+      end
+      if use_stop_marker?
+        values[label(values.length)] = stop
       end
       values
     end
@@ -20,80 +26,8 @@ module Pebblebed
       [prefix, i, suffix].compact.join('_')
     end
 
-    def resolve(positions)
-      positions = positions.split('.')
-      positions = truncate_invalid(positions)
-      (0...MAX_DEPTH).map do |i|
-        positions[i]
-      end
-    end
-
-    def truncate_invalid(positions)
-      labels = []
-      (0...MAX_DEPTH).each do |i|
-        break if positions[i].nil?
-        labels << positions[i]
-      end
-      labels
-    end
-
-    if false
-    include Enumerable
-
-    attr_reader :labels, :all
-    def initialize(positions)
-      @all = resolve positions
-      @labels = all.compact
-    end
-
-    def each
-      labels.each {|label| yield label}
-    end
-
-    def [](index)
-      labels[index]
-    end
-
-    def []=(index, value)
-      labels[index] = value
-    end
-
-    def to_s
-      labels.join('.')
-    end
-
-    class << self
-
-      def to_conditions(path)
-        unless Pebblebed::Uid.valid_path?(path)
-          raise ArgumentError.new("Wildcards terminate the path. Invalid path: #{path}")
-        end
-
-        labels = path.split('.')
-        # In a Pebblebed::Uid::WildcardPath, anything after '^' is optional.
-        optional_part = false
-
-        labels.map! do |label|
-          if label =~ /^\^/
-            label.gsub!(/^\^/, '')
-            optional_part = true
-          end
-
-          result = label.include?('|') ? label.split('|') : label
-          result = [label, nil].flatten if optional_part
-          result
-        end
-
-        result = {}
-        (0...MAX_DEPTH).map do |index|
-          break if labels[index] == '*'
-          result[:"label_#{index}"] = labels[index]
-          break if labels[index].nil?
-        end
-        result
-      end
-
-    end
+    def use_stop_marker?
+      stop != NO_MARKER
     end
   end
 
--- spec/labels_spec.rb
+++ spec/labels_spec.rb
@@ -7,9 +7,15 @@ describe Pebblebed::Labels do
     its(:expanded) {  should eq('label_0' => "a", 'label_1' => "b", 'label_2' => "c") }
   end
 
-  describe "customized labels" do
-    subject { Pebblebed::Labels.new('p.r.q', :prefix => 'dot', :suffix => '') }
-    its(:expanded) { should eq('dot_0_' => 'p', 'dot_1_' => 'r', 'dot_2_' => 'q') }
+  describe "with a stop label" do
+    subject { Pebblebed::Labels.new('a.b.c', :stop => nil) }
+    its(:expanded) {  should eq('label_0' => "a", 'label_1' => "b", 'label_2' => "c", 'label_3' => nil) }
   end
+
+  describe "customized labels" do
+    subject { Pebblebed::Labels.new('p.r.q', :prefix => 'dot', :suffix => '', :stop => '<END>') }
+    its(:expanded) { should eq('dot_0_' => 'p', 'dot_1_' => 'r', 'dot_2_' => 'q', 'dot_3_' => '<END>') }
+  end
+
 
 end
