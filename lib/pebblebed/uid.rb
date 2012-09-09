6bba6e960d76a966d1ed7cbd2c3edcb53a271610
Katrina Owen <katrina.owen@gmail.com>
2012-09-09 18:54:11 +0200

    Clarify comment

--- lib/pebblebed/uid.rb
+++ lib/pebblebed/uid.rb
@@ -116,29 +116,31 @@ module Pebblebed
           stars_are_solitary?(path) && pipes_are_interleaved?(path) && carets_are_leading?(path) && stars_are_terminating?(path)
         end
 
-        # a.*.c is valid
+        # a.*.c is accepted
         # a.*b.c is not
+        # A later rule ensures that * always falls at the end of a path
         def stars_are_solitary?(path)
           !path.split('.').any? {|s| s.match(/.+\*|\*.+/)}
         end
 
-        # a.b|c.d is valid
+        # a.b|c.d is accepted
         # a.|b.c is not
         def pipes_are_interleaved?(path)
           !path.split('.').any? {|s| s.match(/^\||\|$/)}
         end
 
-        # a.^b.c is valid
+        # a.^b.c is accepted
         # a.b^c.d is not
         def carets_are_leading?(path)
           !path.split('.').any? {|s| s.match(/.+\^|\^$/) }
         end
 
-        # a.b.* is valid
+        # a.b.* is accepted
         # *.b.c is not
         def stars_are_terminating?(path)
           path !~ /.*\*\.\w/
         end
+
       end
     end
 
